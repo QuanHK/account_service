17ccb988c75cfabba137f4b32a284c9eefa9f7e3
quanhk <caothequan814@gmail.com>
2025-09-27 08:34:44 +0700

    change postgres connection

--- src/main/java/com/accountservice/AccountServiceApplication.java
+++ src/main/java/com/accountservice/AccountServiceApplication.java
@@ -1,12 +1,20 @@
 package com.accountservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @OpenAPIDefinition
 @SpringBootApplication
 public class AccountServiceApplication {
+
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
